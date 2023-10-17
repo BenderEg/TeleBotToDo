c45a4a1dee9a250bbaf20a40db598f001615c9bf
egor nikitin <eanikitin90gmail.com>
2023-10-17 19:55:04 +0400

    fixed dsn string

--- app/db_start.py
+++ app/db_start.py
@@ -6,7 +6,9 @@ from settings import settings
 
 Base = declarative_base(name='content')
 
-dsn = f'postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@{settings.host}:{settings.port_db}/{settings.postgres_db}'
+dsn = f'''postgresql+asyncpg://{
+    settings.postgres_user}:{settings.postgres_password}@{
+        settings.host}:{settings.port_db}/{settings.postgres_db}'''
 engine = create_async_engine(dsn, echo=True, future=True)
 async_session = sessionmaker(engine, class_=AsyncSession,
                              expire_on_commit=False)
